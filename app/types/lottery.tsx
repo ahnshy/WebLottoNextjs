7c9245616174e801c69071189bdb3c2843e3a1e0
ahnshy <ahnshy@naver.com>
2024-10-03 14:07:48 +0900

    <feat> : type define lottery

---
+++ app/types/lottery.tsx
@@ -0,0 +1,16 @@
+export interface LotteryType {
+  returnValue: string;
+  drwNo: number;
+  drwNoDate: string;
+  totSellamnt: number;
+  firstWinamnt: number;
+  firstPrzwnerCo: number;
+  firstAccumamnt: number;
+  drwtNo1: number;
+  drwtNo2: number;
+  drwtNo3: number;
+  drwtNo4: number;
+  drwtNo5: number;
+  drwtNo6: number;
+  bnusNo: number;
+}
