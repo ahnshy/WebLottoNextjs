1a569ce48543167fb3bbbcf9f2ee779c3f5e5c9f
ahnshy <ahnshy@naver.com>
2024-10-04 19:20:27 +0900

    <feat> : Implementation of probability-based extraction server api of frequency of appearance

--- app/api/lotto/extract/route.tsx
+++ app/api/lotto/extract/route.tsx
@@ -1,30 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
-//import {getAll} from "@/app/database/db";
-//import {MAX_LOTTO_NUMBER, model, trainModel} from "../../../model/sequential";
-//import * as tf from '@tensorflow/tfjs';
+import { trainedModel, predictNumbers, trainModel} from "@/app/model/neuralnetwork";
+import {getAll} from "@/app/database/db";
+import {calculateNumberFrequency, generatePredictedNumbers, generatePredictedNumbersEx} from "@/app/model/frequency";
+import {LotteryType} from "@/app/types/lottery";
+
 
 export async  function GET(req: NextRequest){
+    let response;
+    try {
+            const lotteryList: LotteryType[] = await getAll();
+            const frequency = calculateNumberFrequency(lotteryList);
+            response = generatePredictedNumbersEx(frequency);
+    } catch (error) {
+        console.error(error);
+        response = 'Internal Server Error';
+    }
 
-    // if (!model) {
-    //     model = await trainModel(); // 모델이 없으면 학습
-    // }
-    //
-    // // 입력값을 가져오기 (이 경우 과거 로또 번호의 평균을 사용)
-    // const inputData = await getAll(); // 모든 과거 데이터 가져오기
-    // const { xs } = await preprocessData(inputData);
-    //
-    // // 과거 데이터에서 하나의 샘플을 선택하여 예측에 사용
-    // const inputTensor = xs.slice([0, 0], [1, 6]); // 첫 번째 샘플을 사용
-    //
-    // const prediction = model.predict(inputTensor) as tf.Tensor;
-    // const predictedNumbers = Array.from(prediction.dataSync()).map(num => Math.round(num * MAX_LOTTO_NUMBER));
-    //
-    // // 중복 제거 및 정렬
-    // const uniquePredictedNumbers = Array.from(new Set(predictedNumbers)).sort((a, b) => a - b).slice(0, 6);
-    //
      const res = {
-         message: 'predict number al sequential model based succeed!',
-         predictedNumbers: ""
+         message: 'Frequency numbers based predict succeed!',
+         predictedNumbers: response
     }
     return NextResponse.json(res, { status: 200 });
 }
