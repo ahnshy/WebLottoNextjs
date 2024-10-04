9681adf00f57de1e5d62b098e1bf9bcb693f11a3
ahnshy <ahnshy@naver.com>
2024-10-04 21:18:23 +0900

    <fix> : Change data structure to set to prevent duplicate numbers from being extracted

--- app/api/lotto/predict/route.tsx
+++ app/api/lotto/predict/route.tsx
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getWinNo } from "@/app/database/db";
-import { MAX_LOTTO_NUMBER, trainModel } from "../../../model/sequential";
+import { trainModel } from "../../../model/sequential";
+import { MAX_LOTTO_NUMBER } from "../../../types/lottery";
 import * as tf from '@tensorflow/tfjs';
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
@@ -8,25 +9,25 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
 
   const inputTensor = tf.randomNormal([1, 6]); // 랜덤 텐서
 
-  let uniquePredictedNumbers: number[] = [];
+  let uniquePredictedNumbers: Set<number> = new Set();
 
   // 모델로부터 고유한 숫자 6개를 생성
-  while (uniquePredictedNumbers.length < 6) {
+  while (uniquePredictedNumbers.size < 6) {
     const prediction = model.predict(inputTensor) as tf.Tensor;
     const predictedNumbers: number[] = Array.from(prediction.dataSync()).map(num => Math.round(num * MAX_LOTTO_NUMBER));
 
-    // 중복된 숫자를 제거
-    uniquePredictedNumbers = Array.from(new Set(uniquePredictedNumbers.concat(predictedNumbers)))
-        .map(num => Math.max(1, Math.min(num, MAX_LOTTO_NUMBER))) // 1~45 사이로 보정
-        .sort((a, b) => a - b); // 정렬
-
-    // 6개로 제한
-    uniquePredictedNumbers = uniquePredictedNumbers.slice(0, 6);
+    // 예측된 숫자를 Set에 추가하여 중복 제거
+    predictedNumbers.forEach(num => {
+      const boundedNum = Math.max(1, Math.min(num, MAX_LOTTO_NUMBER)); // 1~45 사이로 보정
+      uniquePredictedNumbers.add(boundedNum);
+    });
   }
+
+  const finalNumbers = Array.from(uniquePredictedNumbers).sort((a, b) => a - b).slice(0, 6);
 
   const res = {
     message: 'Predict number from sequential model succeeded!',
-    data: uniquePredictedNumbers
+    data: finalNumbers
   };
   return NextResponse.json(res, { status: 200 });
 }
