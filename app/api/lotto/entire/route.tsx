d8b86cb3025f51520bc3b4c16f1d325767c0f544
ahnshy <ahnshy@naver.com>
2024-10-04 21:25:34 +0900

    <fix> : modify response message <fix> : Change to clear type definition code

--- app/api/lotto/entire/route.tsx
+++ app/api/lotto/entire/route.tsx
@@ -3,7 +3,7 @@ import {getAll} from "@/app/database/db";
 import {LotteryType} from "@/app/types/lottery";
 
 export async  function GET(req: NextRequest){
-    let response;
+    let response: LotteryType[] | string;
     try {
         const lotteryList: LotteryType[] = await getAll();
         response = lotteryList;
@@ -13,7 +13,7 @@ export async  function GET(req: NextRequest){
     }
 
     const res = {
-        message: 'Frequency numbers based predict succeed!',
+        message: 'Entire number list succeed!',
         predictedNumbers: response
     }
     return NextResponse.json(res, { status: 200 });
