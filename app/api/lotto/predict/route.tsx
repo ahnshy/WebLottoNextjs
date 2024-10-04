2f5116377875e74f25ee5027d6b5706fb7c672a5
ahnshy <ahnshy@naver.com>
2024-10-04 21:00:44 +0900

    <refactor> : predict code refactoring

--- app/api/lotto/predict/route.tsx
+++ app/api/lotto/predict/route.tsx
@@ -1,23 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
-import {getWinNo} from "@/app/database/db";
-import {MAX_LOTTO_NUMBER, trainModel} from "../../../model/sequential";
+import { getWinNo } from "@/app/database/db";
+import { MAX_LOTTO_NUMBER, trainModel } from "../../../model/sequential";
 import * as tf from '@tensorflow/tfjs';
 
-export async  function GET(req: NextRequest){
-
+export async function GET(req: NextRequest): Promise<NextResponse> {
   const model = await trainModel();
 
-  const randomInput = Array.from({ length: 6 }, () => Math.floor(Math.random() * MAX_LOTTO_NUMBER) + 1);
-  const inputTensor = tf.tensor2d([randomInput.map(num => num / MAX_LOTTO_NUMBER)]);
+  const inputTensor = tf.randomNormal([1, 6]); // 랜덤 텐서
 
-  const prediction = model.predict(inputTensor) as tf.Tensor;
-  const predictedNumbers = Array.from(prediction.dataSync()).map(num => Math.round(num * MAX_LOTTO_NUMBER));
+  let uniquePredictedNumbers: number[] = [];
 
-  const uniquePredictedNumbers = Array.from(new Set(predictedNumbers)).sort((a, b) => a - b).slice(0, 6);
+  // 모델로부터 고유한 숫자 6개를 생성
+  while (uniquePredictedNumbers.length < 6) {
+    const prediction = model.predict(inputTensor) as tf.Tensor;
+    const predictedNumbers: number[] = Array.from(prediction.dataSync()).map(num => Math.round(num * MAX_LOTTO_NUMBER));
+
+    // 중복된 숫자를 제거
+    uniquePredictedNumbers = Array.from(new Set(uniquePredictedNumbers.concat(predictedNumbers)))
+        .map(num => Math.max(1, Math.min(num, MAX_LOTTO_NUMBER))) // 1~45 사이로 보정
+        .sort((a, b) => a - b); // 정렬
+
+    // 6개로 제한
+    uniquePredictedNumbers = uniquePredictedNumbers.slice(0, 6);
+  }
 
   const res = {
-    message: 'predict number al sequential model based succeed!',
+    message: 'Predict number from sequential model succeeded!',
     data: uniquePredictedNumbers
-  }
+  };
   return NextResponse.json(res, { status: 200 });
 }
