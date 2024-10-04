536d48b067b9fbe5c7fc5133e7d0c498b90ea69c
ahnshy <ahnshy@naver.com>
2024-10-04 21:20:32 +0900

    <fix> : Change data structure to set to prevent duplicate numbers from being extracted <refactor> : modift import due to move to types MAX_LOTTO_NUMBER

--- app/api/lotto/random/route.tsx
+++ app/api/lotto/random/route.tsx
@@ -1,13 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import {MAX_LOTTO_NUMBER, trainModel} from "../../../model/sequential";
+import { MAX_LOTTO_NUMBER } from "../../../types/lottery";
 
-export async  function GET(req: NextRequest){
+export async function GET(req: NextRequest): Promise<NextResponse> {
+    const uniqueRandomNumbers: Set<number> = new Set();
 
-    const randomInput = Array.from({ length: 6 }, () => Math.floor(Math.random() * MAX_LOTTO_NUMBER) + 1);
+    while (uniqueRandomNumbers.size < 6) {
+        const randomNum = Math.floor(Math.random() * MAX_LOTTO_NUMBER) + 1;
+        uniqueRandomNumbers.add(randomNum);
+    }
+
+    const randomNumbers = Array.from(uniqueRandomNumbers).sort((a, b) => a - b);
 
     const res = {
-        message: 'predict number al sequential model based succeed!',
-        data: randomInput
-    }
+        message: 'Random numbers generated successfully!',
+        data: randomNumbers
+    };
     return NextResponse.json(res, { status: 200 });
 }
