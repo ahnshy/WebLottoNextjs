ff4e22135296ee13960b1c5e834570e7ce99f0c4
ahnshy <ahnshy@naver.com>
2024-10-04 21:25:41 +0900

    <fix> : Change to clear type definition code

--- app/api/lotto/random/route.tsx
+++ app/api/lotto/random/route.tsx
@@ -2,14 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { MAX_LOTTO_NUMBER } from "../../../types/lottery";
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
-    const uniqueRandomNumbers: Set<number> = new Set();
+    const uniqueRandomNumbers: Set<number> = new Set<number>();
 
     while (uniqueRandomNumbers.size < 6) {
         const randomNum = Math.floor(Math.random() * MAX_LOTTO_NUMBER) + 1;
         uniqueRandomNumbers.add(randomNum);
     }
 
-    const randomNumbers = Array.from(uniqueRandomNumbers).sort((a, b) => a - b);
+    const randomNumbers : number[] = Array.from(uniqueRandomNumbers).sort((a, b) => a - b);
 
     const res = {
         message: 'Random numbers generated successfully!',
