185d2fdead4881cd081c078149209f7dc7a6ce7f
ahnshy <ahnshy@naver.com>
2024-10-09 09:51:43 +0900

    <fix> : vercel static page wait for 60sec. error due to over time. function off

--- app/api/lotto/cmd/route.tsx
+++ app/api/lotto/cmd/route.tsx
@@ -1,35 +1,35 @@
 import { NextRequest, NextResponse } from "next/server"
-import { insertData } from '../../../database/db';
+//import { insertData } from '../../../database/db';
 
 export async  function GET(req: NextRequest){
-    const results = [];
+    const results : any = [];
 
-    for (let drwNo = 1; ; drwNo++) {
-        const url = `https://dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${drwNo}`;
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Failed to fetch data for draw number ${drwNo}`);
-            }
-            const data = await response.json();
-
-            if (data.returnValue === "fail") {
-                console.log(`Stopping fetch at draw number ${drwNo} due to failure.`);
-                break;
-            }
-            results.push(data);
-
-        } catch (error) {
-            console.error(`Error fetching data for draw number ${drwNo}:`, error);
-            const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-            results.push({drwNo, error: errorMessage});
-            break;
-        }
-    }
+    // for (let drwNo = 1; ; drwNo++) {
+    //     const url = `https://dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${drwNo}`;
+    //     try {
+    //         const response = await fetch(url);
+    //         if (!response.ok) {
+    //             throw new Error(`Failed to fetch data for draw number ${drwNo}`);
+    //         }
+    //         const data = await response.json();
+    //
+    //         if (data.returnValue === "fail") {
+    //             console.log(`Stopping fetch at draw number ${drwNo} due to failure.`);
+    //             break;
+    //         }
+    //         results.push(data);
+    //
+    //     } catch (error) {
+    //         console.error(`Error fetching data for draw number ${drwNo}:`, error);
+    //         const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+    //         results.push({drwNo, error: errorMessage});
+    //         break;
+    //     }
+    // }
 
     let res, numberStatus;
     try {
-        await insertData(results);
+        //await insertData(results);
         res = {
             message: 'sync all lotto lists success!',
             data: results
