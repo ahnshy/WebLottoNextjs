ca5de921d9768104912ca23f4f1422499b941278
ahnshy <ahnshy@naver.com>
2024-10-09 09:27:15 +0900

    <fix> : Tensorflow prediction results and model training are not sufficient, so annotation is done.

--- app/api/lotto/extract/route.tsx
+++ app/api/lotto/extract/route.tsx
@@ -1,20 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import {getAll} from "@/app/database/db";
-import {calculateNumberFrequency, generatePredictedNumbersEx} from "@/app/model/frequency";
-import {LotteryType} from "@/app/types/lottery";
+//import {getAll} from "@/app/database/db";
+//import {calculateNumberFrequency, generatePredictedNumbersEx} from "@/app/model/frequency";
+//import {LotteryType} from "@/app/types/lottery";
 
 
 export async  function GET(req: NextRequest){
     let response;
-    try {
-            const lotteryList: LotteryType[] = await getAll();
-            const frequency = calculateNumberFrequency(lotteryList);
-            response = generatePredictedNumbersEx(frequency);
-    } catch (error) {
-        console.error(error);
-        response = 'Internal Server Error';
-    }
-
+    //
+    // try {
+    //         const lotteryList: LotteryType[] = await getAll();
+    //         const frequency = calculateNumberFrequency(lotteryList);
+    //         response = generatePredictedNumbersEx(frequency);
+    // } catch (error) {
+    //     console.error(error);
+    //     response = 'Internal Server Error';
+    // }
      const res = {
          message: 'Frequency numbers based predict succeed!',
          predictedNumbers: response
