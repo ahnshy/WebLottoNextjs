9fb0e96ba4f720a6555edfde9911e4b14baf7a19
ahnshy <ahnshy@naver.com>
2024-10-09 10:01:47 +0900

    <refactor> : Separate the basic function for extracting random numbers and RESTful api implementation

--- app/api/lotto/random/route.tsx
+++ app/api/lotto/random/route.tsx
@@ -1,19 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
 import { MAX_LOTTO_NUMBER } from "../../../types/lottery";
 
-export async function GET(req: NextRequest): Promise<NextResponse> {
+import { NextRequest, NextResponse } from "next/server";
+import { MAX_LOTTO_NUMBER } from "../../../types/lottery";
+
+// 난수 생성 함수
+const generateUniqueRandomNumbers = (count: number, max: number): number[] => {
     const uniqueRandomNumbers: Set<number> = new Set<number>();
 
-    while (uniqueRandomNumbers.size < 6) {
-        const randomNum = Math.floor(Math.random() * MAX_LOTTO_NUMBER) + 1;
+    while (uniqueRandomNumbers.size < count) {
+        const randomNum: number = Math.floor(Math.random() * max) + 1;
         uniqueRandomNumbers.add(randomNum);
     }
 
-    const randomNumbers : number[] = Array.from(uniqueRandomNumbers).sort((a, b) => a - b);
+    return Array.from(uniqueRandomNumbers).sort((a, b) => a - b);
+};
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+    const randomNumbers: number[] = generateUniqueRandomNumbers(6, MAX_LOTTO_NUMBER);
 
     const res = {
         message: 'Random numbers generated successfully!',
         data: randomNumbers
     };
+
     return NextResponse.json(res, { status: 200 });
 }
+
+// export async function GET(req: NextRequest): Promise<NextResponse> {
+//     const uniqueRandomNumbers: Set<number> = new Set<number>();
+//
+//     while (uniqueRandomNumbers.size < 6) {
+//         const randomNum = Math.floor(Math.random() * MAX_LOTTO_NUMBER) + 1;
+//         uniqueRandomNumbers.add(randomNum);
+//     }
+//
+//     const randomNumbers : number[] = Array.from(uniqueRandomNumbers).sort((a, b) => a - b);
+//
+//     const res = {
+//         message: 'Random numbers generated successfully!',
+//         data: randomNumbers
+//     };
+//     return NextResponse.json(res, { status: 200 });
+// }
+
