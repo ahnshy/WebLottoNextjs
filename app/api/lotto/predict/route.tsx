ecbeec6a43389f6b7bc701c4cc29c66110130385
ahnshy <ahnshy@naver.com>
2024-10-09 09:29:57 +0900

    <fix> : Tensorflow prediction results and model training are not sufficient, so annotation is done.

--- app/api/lotto/predict/route.tsx
+++ app/api/lotto/predict/route.tsx
@@ -1,29 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getWinNo } from "@/app/database/db";
-import { trainModel } from "../../../model/sequential";
-import { MAX_LOTTO_NUMBER } from "../../../types/lottery";
-import * as tf from '@tensorflow/tfjs';
+//import { getWinNo } from "@/app/database/db";
+//import { trainModel } from "../../../model/sequential";
+//import { MAX_LOTTO_NUMBER } from "../../../types/lottery";
+//import * as tf from '@tensorflow/tfjs';
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
-  const model = await trainModel();
+  let finalNumbers;
+  // const model = await trainModel();
+  //
+  // const inputTensor = tf.randomNormal([1, 6]); // 랜덤 텐서
+  //
+  // let uniquePredictedNumbers: Set<number> = new Set();
+  //
+  // // 모델로부터 고유한 숫자 6개를 생성
+  // while (uniquePredictedNumbers.size < 6) {
+  //   const prediction = model.predict(inputTensor) as tf.Tensor;
+  //   const predictedNumbers: number[] = Array.from(prediction.dataSync()).map(num => Math.round(num * MAX_LOTTO_NUMBER));
+  //
+  //   // 예측된 숫자를 Set에 추가하여 중복 제거
+  //   predictedNumbers.forEach(num => {
+  //     const boundedNum = Math.max(1, Math.min(num, MAX_LOTTO_NUMBER)); // 1~45 사이로 보정
+  //     uniquePredictedNumbers.add(boundedNum);
+  //   });
+  // }
+  //
+  // finalNumbers = Array.from(uniquePredictedNumbers).sort((a, b) => a - b).slice(0, 6);
 
-  const inputTensor = tf.randomNormal([1, 6]); // 랜덤 텐서
-
-  let uniquePredictedNumbers: Set<number> = new Set();
-
-  // 모델로부터 고유한 숫자 6개를 생성
-  while (uniquePredictedNumbers.size < 6) {
-    const prediction = model.predict(inputTensor) as tf.Tensor;
-    const predictedNumbers: number[] = Array.from(prediction.dataSync()).map(num => Math.round(num * MAX_LOTTO_NUMBER));
-
-    // 예측된 숫자를 Set에 추가하여 중복 제거
-    predictedNumbers.forEach(num => {
-      const boundedNum = Math.max(1, Math.min(num, MAX_LOTTO_NUMBER)); // 1~45 사이로 보정
-      uniquePredictedNumbers.add(boundedNum);
-    });
-  }
-
-  const finalNumbers = Array.from(uniquePredictedNumbers).sort((a, b) => a - b).slice(0, 6);
 
   const res = {
     message: 'Predict number from sequential model succeeded!',
