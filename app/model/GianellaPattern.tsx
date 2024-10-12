978851c05cc0e58b42f5fdf7fc9745b52971f6f9
ahnshy <ahnshy@naver.com>
2024-10-12 11:06:16 +0900

    <feat> : refactor typescript regulation

--- app/model/GianellaPattern.tsx
+++ app/model/GianellaPattern.tsx
@@ -1,8 +1,8 @@
-import {MAX_LOTTO_NUMBER} from "@/app/types/lottery";
+import { MAX_LOTTO_NUMBER } from "@/app/types/lottery";
 
-export const generateGianellaNumbers = () => {
-    const numbers = [];
-    const baseNumbers = Array.from({ length: MAX_LOTTO_NUMBER }, (_, i) => i + 1);
+export const GetGianellaNumbers = (): number[] => {
+    const numbers: number[] = [];
+    const baseNumbers: number[] = Array.from({ length: MAX_LOTTO_NUMBER }, (_, i) => i + 1);
 
     for (let i = 0; i < 6; i++) {
         const randomIndex = Math.floor(Math.random() * baseNumbers.length);
