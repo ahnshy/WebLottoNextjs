c35efdcbb2ac5953d3ff36e0594e6c2ee5d308af
ahnshy <ahnshy@naver.com>
2024-10-12 11:10:27 +0900

    <refactor> : rename and replace class library

--- app/model/GianellaPattern.tsx
+++ app/model/GianellaPattern.tsx
@@ -1,29 +1,29 @@
 import { MAX_LOTTO_NUMBER } from "@/app/types/lottery";
 
-class LottoGenerator {
-    private static instance: LottoGenerator;
+class GianellaPattern {
+    private static instance: GianellaPattern;
 
     private constructor() {}
 
-    public static getInstance(): LottoGenerator {
-        if (!LottoGenerator.instance) {
-            LottoGenerator.instance = new LottoGenerator();
+    public static getInstance(): GianellaPattern {
+        if (!GianellaPattern.instance) {
+            GianellaPattern.instance = new GianellaPattern();
         }
-        return LottoGenerator.instance;
+        return GianellaPattern.instance;
     }
 
-    public generateGianellaNumbers(): number[] {
+    public generateNumbers(): number[] {
         const numbers: number[] = [];
         const baseNumbers: number[] = Array.from({ length: MAX_LOTTO_NUMBER }, (_, i) => i + 1);
 
         for (let i = 0; i < 6; i++) {
-            const randomIndex = Math.floor(Math.random() * baseNumbers.length);
+            const randomIndex: number = Math.floor(Math.random() * baseNumbers.length);
             numbers.push(baseNumbers[randomIndex]);
-            baseNumbers.splice(randomIndex, 1);
+            baseNumbers.splice(randomIndex, 1); // Remove the number to avoid duplicates
         }
 
-        return numbers.sort((a, b) => a - b);
+        return numbers.sort((a, b) => a - b); // Sort the numbers
     }
 }
 
-export default LottoGenerator;
+export default GianellaPattern;
