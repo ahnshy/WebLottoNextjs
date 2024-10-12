7190567af76e0fe138a8e6ced95aac4bcca49c70
ahnshy <ahnshy@naver.com>
2024-10-12 11:07:26 +0900

    <refactor> apply singleton pattern and class library

--- app/model/GianellaPattern.tsx
+++ app/model/GianellaPattern.tsx
@@ -1,14 +1,29 @@
 import { MAX_LOTTO_NUMBER } from "@/app/types/lottery";
 
-export const GetGianellaNumbers = (): number[] => {
-    const numbers: number[] = [];
-    const baseNumbers: number[] = Array.from({ length: MAX_LOTTO_NUMBER }, (_, i) => i + 1);
+class LottoGenerator {
+    private static instance: LottoGenerator;
 
-    for (let i = 0; i < 6; i++) {
-        const randomIndex = Math.floor(Math.random() * baseNumbers.length);
-        numbers.push(baseNumbers[randomIndex]);
-        baseNumbers.splice(randomIndex, 1);
+    private constructor() {}
+
+    public static getInstance(): LottoGenerator {
+        if (!LottoGenerator.instance) {
+            LottoGenerator.instance = new LottoGenerator();
+        }
+        return LottoGenerator.instance;
     }
 
-    return numbers.sort((a, b) => a - b);
-};
+    public generateGianellaNumbers(): number[] {
+        const numbers: number[] = [];
+        const baseNumbers: number[] = Array.from({ length: MAX_LOTTO_NUMBER }, (_, i) => i + 1);
+
+        for (let i = 0; i < 6; i++) {
+            const randomIndex = Math.floor(Math.random() * baseNumbers.length);
+            numbers.push(baseNumbers[randomIndex]);
+            baseNumbers.splice(randomIndex, 1);
+        }
+
+        return numbers.sort((a, b) => a - b);
+    }
+}
+
+export default LottoGenerator;
