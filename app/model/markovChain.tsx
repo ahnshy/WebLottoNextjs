94e2741fe8eaebf1d34a1a8374804ddc06ffaabf
ahnshy <ahnshy@naver.com>
2024-10-12 10:23:16 +0900

    <refactor> : apply class library and singlton pattern

--- app/model/markovChain.tsx
+++ app/model/markovChain.tsx
@@ -1,28 +1,43 @@
-import {MAX_LOTTO_NUMBER} from "@/app/types/lottery";
+import { MAX_LOTTO_NUMBER } from "@/app/types/lottery";
 
-function getRandomInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+class LotteryNumberGenerator {
+    private static instance: LotteryNumberGenerator;
 
-function markovChainLotteryNumbers(): number[] {
-    const numbers: Set<number> = new Set<number>();
-    let currentNumber: number = getRandomInt(1, MAX_LOTTO_NUMBER);
+    private constructor() {}
 
-    while (numbers.size < 6) {
-        numbers.add(currentNumber);
-
-        const step: number = Math.random() < 0.5 ? -1 : 1;
-        currentNumber += step;
-
-        if (currentNumber < 1) {
-            currentNumber = 1;
-        } else if (currentNumber > MAX_LOTTO_NUMBER) {
-            currentNumber = MAX_LOTTO_NUMBER;
+    public static getInstance(): LotteryNumberGenerator {
+        if (!LotteryNumberGenerator.instance) {
+            LotteryNumberGenerator.instance = new LotteryNumberGenerator();
         }
+        return LotteryNumberGenerator.instance;
     }
 
-    return Array.from(numbers).sort((a, b) => a - b);
+    private getRandomInt(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    public markovChainLotteryNumbers(): number[] {
+        const numbers: Set<number> = new Set<number>();
+        let currentNumber: number = this.getRandomInt(1, MAX_LOTTO_NUMBER);
+
+        while (numbers.size < 6) {
+            numbers.add(currentNumber);
+
+            const step: number = Math.random() < 0.5 ? -1 : 1;
+            currentNumber += step;
+
+            if (currentNumber < 1) {
+                currentNumber = 1;
+            } else if (currentNumber > MAX_LOTTO_NUMBER) {
+                currentNumber = MAX_LOTTO_NUMBER;
+            }
+        }
+
+        return Array.from(numbers).sort((a, b) => a - b);
+    }
 }
 
-//const lottoNumbers: number[] = markovChainLotteryNumbers();
-//console.log("Get Number:", lottoNumbers);
+// Usage
+//const lotteryGenerator = LotteryNumberGenerator.getInstance();
+//const lottoNumbers: number[] = lotteryGenerator.markovChainLotteryNumbers();
+//console.log("Get Number :", lottoNumbers);
